381b99c474ecfe8ddae955c8bea2678394ecf266
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-08-15 21:24:02 +0100

    Vector2f and Vector2u shorthands

--- SpelSyltCommonLibrary/SpelSyltCommonLibrary/Math/Public/Vector2.h
+++ SpelSyltCommonLibrary/SpelSyltCommonLibrary/Math/Public/Vector2.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cmath>
 #include "Math/Public/NumericDefines.h"
+#include "Math/Public/Mat3x3.h"
 
 //------------------------------------------------------------
 
@@ -14,7 +15,7 @@ namespace CL
 		CVector2(T InX, T InY);
 		explicit CVector2(T InXY);
 		CVector2(const CVector2& InOther);
-		explicit CVector2(CVector2&& InOther);
+		CVector2(CVector2&& InOther) noexcept;
 
 		float Length() const;
 		T Length2() const;
@@ -32,6 +33,7 @@ namespace CL
 		CVector2 operator/(const float InRHS) const;
 		CVector2 operator*(const CVector2& InRHS) const;
 		CVector2 operator*(const float InRHS) const;
+		CVector2 operator*(const CMat3x3& InRHS) const;
 		bool operator==(const CVector2& InRHS) const;
 
 		//Modifier Operators
@@ -42,10 +44,19 @@ namespace CL
 		void operator/=(const float InRHS);
 		void operator*=(const CVector2& InRHS);
 		void operator*=(const float InRHS);
+		void operator*=(const CMat3x3& InRHS);
 	
 		T X;
 		T Y;
 	};
+}
+
+//------------------------------------------------------------
+
+namespace CL
+{
+	typedef CVector2<float> CVector2f;
+	typedef CVector2<unsigned int> CVector2u;
 }
 
 //------------------------------------------------------------
@@ -87,7 +98,7 @@ inline CL::CVector2<T>::CVector2(const CVector2& InOther)
 //------------------------------------------------------------
 
 template<typename T>
-inline CL::CVector2<T>::CVector2(CVector2&& InOther)
+inline CL::CVector2<T>::CVector2(CVector2&& InOther) noexcept
 	: X(std::move(InOther.X))
 	, Y(std::move(InOther.Y))
 {
@@ -192,6 +203,17 @@ inline CL::CVector2<T> CL::CVector2<T>::operator*(const float InRHS) const
 //------------------------------------------------------------
 
 template<typename T>
+inline CL::CVector2<T> CL::CVector2<T>::operator*(const CMat3x3& InRHS) const
+{
+	const float Z = 0.f; //We fake Z for Vec2 * Mat3x3 due to R = C rule
+	const float CalcX = ( X * InRHS.M11() ) + ( Y * InRHS.M12() ) + ( Z * InRHS.M13() );
+	const float CalcY = ( X * InRHS.M21() ) + ( Y * InRHS.M22() ) + ( Z * InRHS.M23() );
+	return CL::CVector2<T>(static_cast<T>(CalcX), static_cast<T>(CalcY));
+}
+
+//------------------------------------------------------------
+
+template<typename T>
 inline bool CL::CVector2<T>::operator==(const CVector2& InRHS) const
 {
 	if constexpr (std::is_floating_point_v<T>)
@@ -257,6 +279,14 @@ inline void CL::CVector2<T>::operator*=(const CVector2& InRHS)
 
 template<typename T>
 inline void CL::CVector2<T>::operator*=(const float InRHS)
+{
+	operator=(operator*(InRHS));
+}
+
+//------------------------------------------------------------
+
+template<typename T>
+inline void CL::CVector2<T>::operator*=(const CMat3x3& InRHS)
 {
 	operator=(operator*(InRHS));
 }

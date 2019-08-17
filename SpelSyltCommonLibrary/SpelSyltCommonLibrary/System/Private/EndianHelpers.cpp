aef5e9601dd0de301e34865e902f0e1f5cca797b
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-08-17 16:33:39 +0100

    Added helpers for endian values

---
+++ SpelSyltCommonLibrary/SpelSyltCommonLibrary/System/Private/EndianHelpers.cpp
@@ -0,0 +1,76 @@
+#include <SpelSyltCommonLibrary/System/Public/EndianHelpers.h>
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <intrin.h>
+
+//------------------------------------------------------------
+
+CL::ESystemEndianType CL::GetSystemEndianType()
+{
+	union
+	{
+		uint32_t asInt;
+		char asByteArray[4];
+	} intVal = {0x01020304};
+
+	return intVal.asByteArray[0] == 1 ? ESystemEndianType::Big : ESystemEndianType::Little;
+}
+
+//------------------------------------------------------------
+
+short CL::SwapEndian(const short InSwapSrc)
+{
+	return _byteswap_ushort(InSwapSrc);
+}
+
+//------------------------------------------------------------
+
+unsigned short CL::SwapEndian(const unsigned short InSwapSrc)
+{
+	return _byteswap_ushort(InSwapSrc);
+}
+
+//------------------------------------------------------------
+
+int CL::SwapEndian(const int InSwapSrc)
+{
+	return _byteswap_ulong(InSwapSrc);
+}
+
+//------------------------------------------------------------
+
+unsigned int CL::SwapEndian(const unsigned int InSwapSrc)
+{
+	return _byteswap_ulong(InSwapSrc);
+}
+
+//------------------------------------------------------------
+
+long CL::SwapEndian(const long InSwapSrc)
+{
+	return _byteswap_ulong(InSwapSrc);
+}
+
+//------------------------------------------------------------
+
+unsigned long CL::SwapEndian(const unsigned long InSwapSrc)
+{
+	return _byteswap_ulong(InSwapSrc);
+}
+
+//------------------------------------------------------------
+
+long long CL::SwapEndian(const long long InSwapSrc)
+{
+	return _byteswap_uint64(InSwapSrc);
+}
+
+//------------------------------------------------------------
+
+unsigned long long CL::SwapEndian(const unsigned long long InSwapSrc)
+{
+	return _byteswap_uint64(InSwapSrc);
+}
+
+//------------------------------------------------------------

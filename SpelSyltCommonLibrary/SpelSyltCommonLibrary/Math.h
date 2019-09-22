e51c0e570e704557e7b68524aab263843cd41916
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-09-22 12:15:24 +0100

    Add Mat4x4 to Math header

--- SpelSyltCommonLibrary/SpelSyltCommonLibrary/Math.h
+++ SpelSyltCommonLibrary/SpelSyltCommonLibrary/Math.h
@@ -4,6 +4,7 @@
 #include <SpelSyltCommonLibrary/Math/Public/NumericDefines.h>
 
 #include <SpelSyltCommonLibrary/Math/Public/Mat3x3.h>
+#include <SpelSyltCommonLibrary/Math/Public/Mat4x4.h>
 
 #include <SpelSyltCommonLibrary/Math/Public/Vector2.h>
 #include <SpelSyltCommonLibrary/Math/Public/Vector3.h>
